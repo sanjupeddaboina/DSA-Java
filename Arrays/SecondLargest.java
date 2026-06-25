2e3ffe0089389d9bf66d6a2e956a6570ea6935c6
SANJAY KUMAR PEDDABOINA <sanjuomg8@gmail.com>
2026-06-25 23:12:43 +0530

    Implement method to find second largest in array

--- Arrays/SecondLargest.java
+++ Arrays/SecondLargest.java
@@ -1,1 +1,20 @@
-
+class Solution{
+  public static int secondLargest(int[] arr, int n) {
+    if (n < 2){
+      return -1;
+    }
+  
+    int large = Integer.MIN_VALUE, second_large = Integer.MIN_VALUE;
+  
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > large) {
+            second_large = large;
+            large = arr[i];
+        } 
+        else if (arr[i] > second_large && arr[i] != large) {
+            second_large = arr[i];
+        }
+    }
+    return second_large;
+  }
+}
