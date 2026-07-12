678c5ad3bd81da5381fabfee2c3fac6d511a3ba6
SANJAY KUMAR PEDDABOINA <sanjuomg8@gmail.com>
2026-07-13 00:01:32 +0530

    Add LowerBoundFinder class with lowerBound method

---
+++ Binary Search/Upper bound.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class LowerBoundFinder {
+    public int lowerBound(int[] arr, int x) {
+        int low = 0;
+        int high = arr.length - 1;
+        int ans = arr.length;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] >= x) {
+                ans = mid;            
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
