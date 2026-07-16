324864bf4b54847747e067611764e60355a6e6c1
SANJAY KUMAR PEDDABOINA <sanjuomg8@gmail.com>
2026-07-16 23:35:16 +0530

    Create Count rotations.java

---
+++ Binary Search/Count rotations.java
@@ -0,0 +1,18 @@
+import.java.util.*;
+class Solution {
+    public int findRotations(int[] arr) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] > arr[high]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+}
