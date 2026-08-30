fd84128186c93d4f6df2a8ba955582dd23dafbbe
SANJAY KUMAR PEDDABOINA <sanjuomg8@gmail.com>
2026-08-30 23:58:31 +0530

    Add solution for final prices with special discounts
    Implement solution for calculating final prices with discounts based on future prices.

---
+++ Stacks/Monotonic Stack/Final Prices With a Special Discount in a Shop.java
@@ -0,0 +1,31 @@
+/* Final Prices With a Special Discount in a Shop:
+You are given an integer array prices where prices[i] is the price of the ith item in a shop.
+There is a special discount for items in the shop. If you buy the ith item, then you will receive a discount equivalent to 
+prices[j] where j is the minimum index such that j > i and prices[j] <= prices[i]. Otherwise, you will not receive any discount at all.
+Return an integer array answer where answer[i] is the final price you will pay for the ith item of the shop, considering the special discount.
+*/
+
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int n = prices.length;
+
+        Stack<Integer> st = new Stack<>();
+        int[] ans = new int[n];
+
+        for(int i = n - 1; i >= 0; i--){
+
+            while(!st.isEmpty() && st.peek() > prices[i]){
+                st.pop();
+            }
+            
+            if(st.isEmpty()){
+                ans[i] = prices[i];
+            }else{
+                ans[i] = prices[i] - st.peek();
+            }
+
+            st.push(prices[i]);
+        }
+        return ans;
+    }
+}
