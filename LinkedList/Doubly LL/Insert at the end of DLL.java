128e718d92498653abcc351f6a738992bb31622d
SANJAY KUMAR PEDDABOINA <sanjuomg8@gmail.com>
2026-09-19 00:01:00 +0530

    Update Insert at the end of DLL.java

--- LinkedList/Doubly LL/Insert at the end of DLL.java
+++ LinkedList/Doubly LL/Insert at the end of DLL.java
@@ -1,22 +1,22 @@
- public class Solution { 
-  public static Node insertAtTail(Node head, int k) {
-          // Create a new node with data 'k'
-    Node newNode = new Node(k);
-
-    // If the list is empty, return the new node as the head
-    if (head == null) {
-        return newNode;
-    }
-
-    // Traverse to the last node of the doubly linked list
-    Node tail = head;
-    while (tail.next != null) {
-        tail = tail.next;
-    }
-
-    // Connect the new node to the last node
-    tail.next = newNode;
-    newNode.back = tail;  // Set the 'back' pointer of the new node to the previous node
-    return head;  // Return the head of the modified list
-  }
+class Solution { 
+   public static Node insertAtTail(Node head, int k) {
+           // Create a new node with data 'k'
+      Node newNode = new Node(k);
+  
+      // If the list is empty, return the new node as the head
+      if (head == null) {
+          return newNode;
+      }
+  
+      // Traverse to the last node of the doubly linked list
+      Node tail = head;
+      while (tail.next != null) {
+          tail = tail.next;
+      }
+  
+      // Connect the new node to the last node
+      tail.next = newNode;
+      newNode.back = tail;  // Set the 'back' pointer of the new node to the previous node
+      return head;  // Return the head of the modified list
+   }
 }
