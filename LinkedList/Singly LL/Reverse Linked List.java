48cf9132177504cd89cf6ccf492e111b1dbff298
SANJAY KUMAR PEDDABOINA <sanjuomg8@gmail.com>
2026-09-19 23:38:59 +0530

    Implement reverse linked list function

---
+++ LinkedList/Singly LL/Reverse Linked List.java
@@ -0,0 +1,14 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode next = null;
+        
+        while(head != null){
+            next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
