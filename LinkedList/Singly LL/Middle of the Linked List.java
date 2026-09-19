6a5b160f1272a0b1237c89ac3440c5cc387d950c
SANJAY KUMAR PEDDABOINA <sanjuomg8@gmail.com>
2026-09-19 23:37:41 +0530

    Create Middle of the Linked List.java

---
+++ LinkedList/Singly LL/Middle of the Linked List.java
@@ -0,0 +1,17 @@
+/* Middle of the LinkedList:
+Given the head of a singly linked list, return the middle node of the linked list.
+If there are two middle nodes, return the second middle node.
+*/
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
